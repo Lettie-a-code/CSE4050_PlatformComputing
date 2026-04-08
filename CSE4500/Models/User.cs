ff6c264ffc570826ee3b075a1ca19bf235c86657
Lettie-a-code <008720420.coyote.csusb.edu>
2026-04-08 12:45:03 -0700

    Updated repo

--- CSE4500/Models/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CSE4050_SignIn.Models;
-public class User
-{
-    public int Id { get; set; }
-    public string Email { get; set; } = "";
-    public string PasswordHash { get; set; } = "";
-}
--- CSE4500/ViewModels/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace CSE4050_SignIn.ViewModels;
-public class LoginViewModel
-{
-    [Required, EmailAddress]
-    public string Email { get; set; } = "";
-    [Required]
-    public string Password { get; set; } = "";
-}
